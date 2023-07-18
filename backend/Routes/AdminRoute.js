66070d66691408d4df231da796d2730048d52c2e
ReshnaRaj <reshnarajan84@gmail.com>
2023-07-18 18:50:31 +0530

    new

--- backend/Routes/AdminRoute.js
+++ backend/Routes/AdminRoute.js
@@ -1,24 +1,38 @@
-const express = require('express')
-const router = express.Router()
-const {adminlogin,isAdminAuth}=require('../Controller/authController')
-const {getallresortdata}=require('../Controller/StaffController')
-const {getuniqueresortdata,getalladvdata,getuniqadvdata,approveAdvent,getalldestdata,getuniquedest,approveDestination,getAllstaffData,blockStaff,rejectResort,approvedresort,getallbookings}=require('../Controller/AdminController')
-const {checkAdmin}=require('../Middleware/authuser')
-router.post('/adlogin',adminlogin)
-router.get('/getallresortdata',getallresortdata)
-router.get('/getuniqueresort/:id',getuniqueresortdata)
+const express = require("express");
+const router = express.Router();
+const { adminlogin, isAdminAuth } = require("../Controller/authController");
+const { getallresortdata } = require("../Controller/StaffController");
+const {
+  approveresort,
+  getuniqueresortdata,
+  getalladvdata,
+  getuniqadvdata,
+  approveAdvent,
+  getalldestdata,
+  getuniquedest,
+  approveDestination,
+  getAllstaffData,
+  blockStaff,
+  rejectResort,
+  approvedresort,
+  getallbookings,
+} = require("../Controller/AdminController.js");
+const { checkAdmin } = require("../Middleware/authuser");
+router.post("/adlogin", adminlogin);
+router.get("/getallresortdata", getallresortdata);
+router.get("/getuniqueresort/:id", getuniqueresortdata);
 // router.post('/approveresort/:id',approveresort)
-router.get('/isAdminauth',checkAdmin,isAdminAuth)
-router.get('/getalladvdata',checkAdmin,getalladvdata)
-router.get('/getuniqadv/:id',checkAdmin,getuniqadvdata)
-router.post('/approveadvent/:id',checkAdmin,approveAdvent)
-router.get('/getalldestdata',checkAdmin,getalldestdata)
-router.get('/getuniqdest/:id',checkAdmin,getuniquedest)
-router.post('/approvedest/:id',checkAdmin,approveDestination)
-router.get('/getAllstaff',checkAdmin,getAllstaffData)
-router.post('/blockstaff/:id',checkAdmin,blockStaff)
-router.post('/rejectresort/:id',checkAdmin,rejectResort)
-router.post('/approvedresort/:id',checkAdmin,approvedresort)
-router.get('/getallbookings',checkAdmin,getallbookings)
+router.get("/isAdminauth", checkAdmin, isAdminAuth);
+router.get("/getalladvdata", checkAdmin, getalladvdata);
+router.get("/getuniqadv/:id", checkAdmin, getuniqadvdata);
+router.post("/approveadvent/:id", checkAdmin, approveAdvent);
+router.get("/getalldestdata", checkAdmin, getalldestdata);
+router.get("/getuniqdest/:id", checkAdmin, getuniquedest);
+router.post("/approvedest/:id", checkAdmin, approveDestination);
+router.get("/getAllstaff", checkAdmin, getAllstaffData);
+router.post("/blockstaff/:id", checkAdmin, blockStaff);
+router.post("/rejectresort/:id", checkAdmin, rejectResort);
+router.post("/approvedresort/:id", checkAdmin, approvedresort);
+router.get("/getallbookings", checkAdmin, getallbookings);
 
-module.exports=router;
+module.exports = router;
